34a68d749ca49efa384446f4ca66feb0a58d12e0
travisen <travis.nykaza@gmail.com>
2016-05-30 12:59:45 -0400

    removed mysql from mysql.pool

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.get('/add-ex', function(req,res){
 
 app.get('/reset-table',function(req,res,next){
   var context = {};
-  mysql.pool.query("DROP TABLE IF EXISTS workouts", function(err){ //replace your connection pool with the your variable containing the connection pool
+  pool.query("DROP TABLE IF EXISTS workouts", function(err){ //replace your connection pool with the your variable containing the connection pool
     var createString = "CREATE TABLE workouts("+
     "id INT PRIMARY KEY AUTO_INCREMENT,"+
     "name VARCHAR(255) NOT NULL,"+
@@ -28,7 +28,7 @@ app.get('/reset-table',function(req,res,next){
     "weight INT,"+
     "date DATE,"+
     "lbs BOOLEAN)";
-    mysql.pool.query(createString, function(err){
+    pool.query(createString, function(err){
       context.results = "Table reset";
       res.render('home',context);
     })
