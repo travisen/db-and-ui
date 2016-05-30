309f9ffade28b3453710bfbf5205826a167e1950
travisen <travis.nykaza@gmail.com>
2016-05-30 12:42:33 -0400

    added sql pool

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.get('/add-ex', function(req,res){
 
 app.get('/reset-table',function(req,res,next){
   var context = {};
-  [your connection pool].query("DROP TABLE IF EXISTS workouts", function(err){ //replace your connection pool with the your variable containing the connection pool
+  mysql.query("DROP TABLE IF EXISTS workouts", function(err){ //replace your connection pool with the your variable containing the connection pool
     var createString = "CREATE TABLE workouts("+
     "id INT PRIMARY KEY AUTO_INCREMENT,"+
     "name VARCHAR(255) NOT NULL,"+
