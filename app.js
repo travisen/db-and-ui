c425716a080e480ff283631252ced5c48e73a3af
travisen <travis.nykaza@gmail.com>
2016-05-30 20:22:35 -0400

    moved code back to code.js

--- app.js
+++ app.js
@@ -12,47 +12,6 @@ app.use(express.static('public'));
 app.get('/',function(req,res){
   res.render('home');
   //document.addEventListener('DOMContentLoaded', bindButtons);
-  function bindButtons(){
-
-  /*submit data button*/
-  document.getElementById('submit-data').addEventListener('click', function(event){
-    var req = new XMLHttpRequest();
-    var payload = {};
-
-    var tableRef = document.getElementById("ex-table").getElementsByTagName("tbody")[0];
-    console.log(tableRef);
-    // insert a row in table at last row
-    var newRow = tableRef.insertRow(tableRef.rows.length);
-
-    //insert a cell in the row
-    var newCell = newRow.insertCell(0);
-
-    //var newText = document.createTextNode('sdsadsadsNew row');
-    //newCell.appendChild(newText);
-
-    payload = document.getElementById('ex-name').value;
-    //console.log(payload);
-    req.open("POST", "http://httpbin.org/post", true);
-
-    req.setRequestHeader('Content-Type', 'application/json');
-
-    req.addEventListener('load', function() {
-      if(req.status >= 200 && req.status < 400) {
-        var response = JSON.parse(req.responseText);
-        //document.getElementById('')
-        console.log(response);
-        var newText = document.createTextNode(response.data); //this'll be changed when sql is added
-        newCell.appendChild(newText);
-      } else {
-        console.log("Error in network request: " + request.statusText);
-      }
-    });
-
-      req.send(payload);
-      event.preventDefault();
-  });
-  /*end submit data button*/
-  }
 });
 
 app.get('/add-ex', function(req,res){
