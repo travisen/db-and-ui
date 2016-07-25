5765144d812a6bf38c39eda8b8b498342726296f
travisen <travis.nykaza@gmail.com>
2016-07-25 13:00:25 -0400

    Add unitType function

--- public/javascript/code.js
+++ public/javascript/code.js
@@ -3,8 +3,7 @@ document.addEventListener('DOMContentLoaded', bindButtons);
 document.addEventListener('DOMContentLoaded', populateTable(1));
 
  /* delete row function */
-function deleteRow(id) {
-	
+function deleteRow(id) {	
 
 	console.log("delete id at: ", id);
 	var row = document.getElementById(id);
@@ -28,14 +27,14 @@ function deleteRow(id) {
 };
 
 /*
-This functions creates buttons for the workout log table.
+This function creates buttons for the workout log table.
 param:
 	workoutObject: response from server.
 	count: current column
 	curRow: current Row
 	nameButton: string that names a button
-	declareAction: javascript (passed in as a string) to set buttons action
-	on a click
+	declareAction: javascript (passed in as a string) to set buttons
+	action on a click
 */
 function createButton(workoutObject, count, curRow ,nameButton, declareAction){
 	var cellButton = document.createElement('td');
@@ -47,8 +46,18 @@ function createButton(workoutObject, count, curRow ,nameButton, declareAction){
 	aButton.setAttribute("onclick", declareAction);
 	curRow.appendChild(cellButton.appendChild(aButton));
 }
+
+
+function unitType(workoutObject, count, cell, colCount){
+	if (workoutObject[count].lbs == 1 && colCount == 6){
+		cell.textContent = "lbs";
+	} else if (workoutObject[count].lbs == 0 && colCount == 6) {
+		cell.textContent = "kgs";
+	}
+}
 /*	
-	// remove this...
+	think about refactoring how population of
+	a table works
 	Populates table and checks for errors
 	popType = 1 populate whole table
 	popType = 2 add one row
@@ -78,58 +87,47 @@ function populateTable(popType) {
 		var tableRef = document.getElementById("ex-table").getElementsByTagName("tbody")[0];
 
 		//debuggin
-		var count =0;
-
+		var count1 =0;
+		var count2 = 0;
 		//loop to populate table
 		var table = document.getElementById("table-body");
 		for (var i = start; i<workoutObject.length; i++){
-			var isId = true;
-			var isUnit = 1;
+			var isId = true; // used id each row of the table. hidden
+			var colCount = 1;
 
+			console.log("row iterations: ", count1);
 			if(workoutObject.length > 0){
 				// Insert a row in tableRef at last row.
 				var newRow = tableRef.insertRow(tableRef.rows.length);
 				newRow.id = workoutObject[i].id;
-
 			}
-
+			// this loop populates a row
 			for (var val in workoutObject[i]){
 				console.log(workoutObject[i][val]);
-
 				var newCell = document.createElement('td');
-
 				newCell.textContent = workoutObject[i][val];
 
-				if (workoutObject[i].lbs == 1 && isUnit == 6){
-					//console.log("workoutObject[i].lbs ", workoutObject[i].lbs)
-					newCell.textContent = "lbs";
-				} else if (workoutObject[i].lbs == 0 && isUnit == 6) {
-					newCell.textContent = "kgs";
-				}
+				//units is on 6th iteration
+				unitType(workoutObject, i, newCell, colCount);
 
 				if(isId == true){
 					newCell.style.display="none"
 					isId = false; 
 				}
 				newRow.appendChild(newCell);
-
-				isUnit++; // keeps track on whether to turn 0 or 1 into lbs or kgs
+				colCount++; // keeps track on whether to turn 0 or 1 into lbs or kgs
 				} // end inner loop
 				
-				console.log("val of I: ", i);
 				createButton(workoutObject, i, newRow,
 				 "delete", "deleteRow(this.id)");
 				createButton(workoutObject, i, newRow,
 				 "update", "window.location='update-row?id='+this.id");
-
 			} // end outer loop
-
 		} else 
 			console.log("Error in network request: " + reqInitial.statusText);
 	});
 	reqInitial.send(null);
 }
-/* end populate table function */
 
 /* binds event listeners to buttons*/
 function bindButtons(){
@@ -149,12 +147,6 @@ function bindButtons(){
 			lbs: null
 		};
 		var tableRef = document.getElementById("ex-table").getElementsByTagName("tbody")[0];
-
-		// insert a row in table at last row
-		//var newRow = tableRef.insertRow(tableRef.rows.length);
-
-		//insert a cell in the row
-		//var newCell = newRow.insertCell(0);
 	
 		// lbs return true, kg return false
 		if (document.getElementById('lbs-true').checked == 'on'){
