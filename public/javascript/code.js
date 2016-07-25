5166f97853b19abb73a0b07bc7845419737c4684
travisen <travis.nykaza@gmail.com>
2016-07-25 12:21:13 -0400

    Add button create function

--- public/javascript/code.js
+++ public/javascript/code.js
@@ -27,103 +27,107 @@ function deleteRow(id) {
 	event.preventDefault();
 };
 
-/* end delete row function */
 /*
+This functions creates buttons for the workout log table.
+param:
+	workoutObject: response from server.
+	count: current column
+	curRow: current Row
+	nameButton: string that names a button
+	declareAction: javascript (passed in as a string) to set buttons action
+	on a click
+*/
+function createButton(workoutObject, count, curRow ,nameButton, declareAction){
+	var cellButton = document.createElement('td');
+	var aButton = document.createElement('button');
+	aButton.class = "button-primary";
+	//deleteButton.style = "marigin-bottom:0rem";
+	aButton.id = workoutObject[count].id;
+	aButton.textContent = nameButton;
+	aButton.setAttribute("onclick", declareAction);
+	curRow.appendChild(cellButton.appendChild(aButton));
+}
+/*	
+	// remove this...
 	Populates table and checks for errors
 	popType = 1 populate whole table
 	popType = 2 add one row
 */
 function populateTable(popType) {
-		var reqInitial = new XMLHttpRequest();
-		reqInitial.open("GET", "http://localhost:3009/select-all", true); //change address
-		reqInitial.setRequestHeader('Content-Type', 'application/json');
-		reqInitial.addEventListener('load', function(event){
-			if(reqInitial.status >= 200 && reqInitial.status < 400){
-			var responseInitial = JSON.parse(reqInitial.responseText);
-			var workoutObject = responseInitial.workouts;
+	var reqInitial = new XMLHttpRequest();
+	reqInitial.open("GET", "http://localhost:3009/select-all", true); //change address
+	reqInitial.setRequestHeader('Content-Type', 'application/json');
+	reqInitial.addEventListener('load', function(event){
+		if(reqInitial.status >= 200 && reqInitial.status < 400){
+		var responseInitial = JSON.parse(reqInitial.responseText);
+		var workoutObject = responseInitial.workouts;
 
-			//populate table initial
-			var workoutObject = responseInitial; // workout server response
-			console.log("length of workout object", workoutObject.length);
+		//populate table initial
+		var workoutObject = responseInitial; // workout server response
+		console.log("length of workout object", workoutObject.length);
 
-			console.log("objects inside", workoutObject);
+		console.log("objects inside", workoutObject);
 
-			if (popType == 1) {
+		// Set starting value for populate table loop
+		if (popType == 1)
 			var start = 0;
-			}
-			else if (popType == 2) {
+		else if (popType == 2)
 			var start = workoutObject.length - 1;
-			console.log("val start", start)
-			console.log("poptype 2", workoutObject[start]);
-			console.log("new length", workoutObject.length);
+	
+		// Reference variable for the entire table.
+		var tableRef = document.getElementById("ex-table").getElementsByTagName("tbody")[0];
+
+		//debuggin
+		var count =0;
+
+		//loop to populate table
+		var table = document.getElementById("table-body");
+		for (var i = start; i<workoutObject.length; i++){
+			var isId = true;
+			var isUnit = 1;
+
+			if(workoutObject.length > 0){
+				// Insert a row in tableRef at last row.
+				var newRow = tableRef.insertRow(tableRef.rows.length);
+				newRow.id = workoutObject[i].id;
+
 			}
 
-			// Reference variable for the entire table.
-			var tableRef = document.getElementById("ex-table").getElementsByTagName("tbody")[0];
+			for (var val in workoutObject[i]){
+				console.log(workoutObject[i][val]);
 
-			//debuggin
-			var count =0;
+				var newCell = document.createElement('td');
 
-			//loop to populate table
-			var table = document.getElementById("table-body");
-			for (var i = start; i<workoutObject.length; i++){
-				var isId = true;
-				var isUnit = 1;
+				newCell.textContent = workoutObject[i][val];
 
-				if(workoutObject.length > 0){
-					// Insert a row in tableRef at last row.
-					var newRow = tableRef.insertRow(tableRef.rows.length);
-					newRow.id = workoutObject[i].id;
-
+				if (workoutObject[i].lbs == 1 && isUnit == 6){
+					//console.log("workoutObject[i].lbs ", workoutObject[i].lbs)
+					newCell.textContent = "lbs";
+				} else if (workoutObject[i].lbs == 0 && isUnit == 6) {
+					newCell.textContent = "kgs";
 				}
 
-				for (var val in workoutObject[i]){
-					console.log(workoutObject[i][val]);
+				if(isId == true){
+					newCell.style.display="none"
+					isId = false; 
+				}
+				newRow.appendChild(newCell);
 
-					var newCell = document.createElement('td');
+				isUnit++; // keeps track on whether to turn 0 or 1 into lbs or kgs
+				} // end inner loop
+				
+				console.log("val of I: ", i);
+				createButton(workoutObject, i, newRow,
+				 "delete", "deleteRow(this.id)");
+				createButton(workoutObject, i, newRow,
+				 "update", "window.location='update-row?id='+this.id");
 
-					newCell.textContent = workoutObject[i][val];
+			} // end outer loop
 
-					if (workoutObject[i].lbs == 1 && isUnit == 6){
-						//console.log("workoutObject[i].lbs ", workoutObject[i].lbs)
-						newCell.textContent = "lbs";
-					} else if (workoutObject[i].lbs == 0 && isUnit == 6) {
-						newCell.textContent = "kgs";
-					}
-
-					if(isId == true){
-						newCell.style.display="none"
-						isId = false; 
-					}
-					newRow.appendChild(newCell);
-
-					isUnit++; // keeps track on whether to turn 0 or 1 into lbs or kgs
-					} // end inner loop
-					
-					var cellButton = document.createElement('td');
-					var deleteButton = document.createElement('button');
-					deleteButton.class = "button-primary";
-					//deleteButton.style = "marigin-bottom:0rem";
-					deleteButton.id = workoutObject[i].id;
-					deleteButton.textContent = "delete";
-					deleteButton.setAttribute("onclick", "deleteRow(this.id)")
-					newRow.appendChild(cellButton.appendChild(deleteButton));
-
-					var cellButton2 = document.createElement('td');
-					var updateButton = document.createElement('button');
-					updateButton.class = "button-primary";
-					updateButton.id = workoutObject[i].id;
-					updateButton.textContent = "update";
-					updateButton.action
-					updateButton.setAttribute("onclick", "window.location='update-row?id='+this.id;");
-
-					newRow.appendChild(cellButton2.appendChild(updateButton));
-				} // end outer loop
-
-			} else 
-				console.log("Error in network request: " + reqInitial.statusText);
-		});
-		reqInitial.send(null);
+		} else 
+			console.log("Error in network request: " + reqInitial.statusText);
+	});
+	reqInitial.send(null);
 }
 /* end populate table function */
 
